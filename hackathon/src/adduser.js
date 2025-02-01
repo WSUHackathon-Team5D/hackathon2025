db34127b7d9d62cd0c6d435a79e6a88c15661345
JamesBennett5027 <111904791+JamesBennett5027@users.noreply.github.com>
2025-01-31 20:29:02 -0500

    Create adduser.js

---
+++ hackathon/src/adduser.js
@@ -0,0 +1,7 @@
+document.querySelector('form').addEventListener('submit', function(evt) {
+
+    evt.preventDefault();
+   let email = document.getElementById('email').value;
+   let password = document.getElementById('password').value;
+   let username = document.getElementById('username').value;
+});
