04f28b4d1ae20fa164aa7344f7345dcc9b02ae51
JamesBennett5027 <111904791+JamesBennett5027@users.noreply.github.com>
2025-02-01 09:21:39 -0500

    Update adduser.js

--- hackathon/src/adduser.js
+++ hackathon/src/adduser.js
@@ -1,7 +1,41 @@
-document.querySelector('form').addEventListener('submit', function(evt) {
+import{ Client} from 'pg';
+const express =require ('express');
+const app =express();
+
+const corsOptions = {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}
+
+app.use(corsOptions);
+const client = new Client({// this is so the script knows the information for the database (yes its hard-coaded I'll fix it later)
+    host:"localhost",
+    user:"user",
+    port:5432,
+    database:app
+});
+// app.use(cors)
+client.connect();
+const query ={
+    text:'SELECT * FROM users',
+};
+document.querySelector('form').addEventListener('submit', async function(evt) {
 
     evt.preventDefault();
-   let email = document.getElementById('email').value;
-   let password = document.getElementById('password').value;
-   let username = document.getElementById('username').value;
+    let newemail = document.getElementById('email').value;
+    let newpassword = document.getElementById('password').value;
+    let newusername = document.getElementById('username').value;
+    let other_users = await client.query(query);
+    let validuser = true;
+    for (let i =0;i<other_users.length;i++){
+        if(newusername == other_users[i].username || newemail == other_users[i].email){
+            validuser = false;
+        }
+    }
+
+    if(validuser){
+    let insertquery = 'INSERT INTO USERS' + newemail + ' , ' + newusername +' , ' + newpassword;
+    clinet.query(insertquery);
+    }
 });
